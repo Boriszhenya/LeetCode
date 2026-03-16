e6759d784c8a6de9d4e771174d70e20a0609335e
Borys Pedorenko <boriszhenya@gmail.com>
2026-03-16 18:14:32 +0100

    add Aufgaben 2710

--- src/Aufgabe2710.java
+++ src/Aufgabe2710.java
@@ -6,7 +6,19 @@ public class Aufgabe2710 {
     }
 
     public static String removeTrailingZeros(String num) {
-        return num;
-    }
+        char[] arr = num.toCharArray();
+        int count = 0;
+        for (int i = num.length() - 1; i >= 0; i--) {
+            if (num.charAt(i) == '0') {
+                count++;
+            } else {
+                break;
+            }
+        }
+        char[] newArr = new char[arr.length - count];
 
+        System.arraycopy(arr, 0, newArr, 0, newArr.length);
+
+        return new String(newArr);
+    }
 }
